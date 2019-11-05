f87a5162db7f9c69b3ac199cdeedcabc3529836d
mac <>
2019-11-05 09:56:32 +0800

    init

--- Example/CDTitleSlideWrapView/LYViewController.m
+++ Example/CDTitleSlideWrapView/LYViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "LYViewController.h"
-
+#import "CDTitleSlideWrapView.h"
 @interface LYViewController ()
 
 @end
@@ -17,13 +17,30 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.navigationController.navigationBar.translucent = NO;
+    
+    CDTitleSlideWrapView *slideWrapView = [CDTitleSlideWrapView new];
+    slideWrapView.frame = self.view.bounds;
+    [self.view addSubview:slideWrapView];
+    
+    NSArray *rawTitles = @[@"title1",@"title2",@"title3"];
+    slideWrapView.items = rawTitles;
+
+    UIViewController *vc1 = [UIViewController new];
+    vc1.view.backgroundColor = [UIColor whiteColor];
+    UIViewController *vc2 = [UIViewController new];
+    vc2.view.backgroundColor = [UIColor whiteColor];
+    UIViewController *vc3 = [UIViewController new];
+    vc3.view.backgroundColor = [UIColor whiteColor];
+    slideWrapView.viewControllers = @[vc1,vc2,vc3];
+    
+////    取消注释试试看
+//    slideWrapView.titleTabView.indicatorColor = [UIColor orangeColor];
+//    slideWrapView.titleTabView.titleTabCollectionView.inActiveColor = [UIColor grayColor];
+//    slideWrapView.titleTabView.titleTabCollectionView.activeColor = [UIColor redColor];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 @end
---
+++ Example/Pods/Target Support Files/CDTitleSlideWrapView/CDTitleSlideWrapView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CDTitleSlideWrapView : NSObject
+@end
+@implementation PodsDummy_CDTitleSlideWrapView
+@end
