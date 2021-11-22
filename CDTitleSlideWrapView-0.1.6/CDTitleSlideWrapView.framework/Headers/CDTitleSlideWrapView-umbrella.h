9069e965470337c99877d51ec40be795e3227b96
ly <lyluoyuan@126.com>
2021-11-22 17:26:33 +0800

    delete

---
+++ CDTitleSlideWrapView-0.1.6/CDTitleSlideWrapView.framework/Headers/CDTitleSlideWrapView-umbrella.h
@@ -0,0 +1,21 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "CDSlideView.h"
+#import "CDTitleSlideWrapView.h"
+#import "CDTitleTabCollectionView.h"
+#import "CDTitleTabCollectionViewCell.h"
+#import "CDTitleTabView.h"
+
+FOUNDATION_EXPORT double CDTitleSlideWrapViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char CDTitleSlideWrapViewVersionString[];
+
--- CDTitleSlideWrapView-0.1.6/CDTitleSlideWrapView.podspec
+++ CDTitleSlideWrapView-0.1.6/CDTitleSlideWrapView.podspec
@@ -30,7 +30,7 @@ TODO: Add long description of the pod here.
 
   s.ios.deployment_target = '8.0'
 
-  s.source_files = 'CDTitleSlideWrapView.xcframework/ios-arm64_i386_x86_64-simulator/**/*'
+  s.source_files = 'CDTitleSlideWrapView.framework'
   
   # s.resource_bundles = {
   #   'CDTitleSlideWrapView' => ['CDTitleSlideWrapView/Assets/*.png']
